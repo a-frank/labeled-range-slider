e903b3fcf8cee60999c1833ad14d90143dddca93
Lex <alex.frank84@gmail.com>
2022-06-03 16:58:46 +0200

    Move slider and modifier to new file and add shadow to touch circle

---
+++ app/src/main/java/de/lex/compose/TouchInteractionModifier.kt
@@ -0,0 +1,41 @@
+package de.lex.compose
+
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.forEachGesture
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.pointer.PointerEvent
+import androidx.compose.ui.input.pointer.PointerInputChange
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.positionChange
+
+
+sealed class TouchInteraction {
+	object NoInteraction : TouchInteraction()
+	data class Down(val position: Offset) : TouchInteraction()
+	data class Move(val position: Offset) : TouchInteraction()
+}
+
+fun Modifier.touchInteraction(key: Any, block: (TouchInteraction) -> Unit): Modifier =
+	pointerInput(key) {
+		forEachGesture {
+			awaitPointerEventScope {
+
+				val down: PointerInputChange = awaitFirstDown()
+				block(TouchInteraction.Down(down.position))
+
+				do {
+					val event: PointerEvent = awaitPointerEvent()
+
+					event.changes
+						.forEach { pointerInputChange: PointerInputChange ->
+							if (pointerInputChange.positionChange() != Offset.Zero) pointerInputChange.consume()
+						}
+
+					block(TouchInteraction.Move(event.changes.first().position))
+				} while (event.changes.any { it.pressed })
+
+				block(TouchInteraction.NoInteraction)
+			}
+		}
+	}
