dc648420d8be16864baea9d909ab6c9f3954afa1
Lex <alex.frank84@gmai.com>
2022-08-05 10:15:34 +0200

    Visualize range changes in the demp app

--- app/src/main/java/de/lex/compose/MainActivity.kt
+++ app/src/main/java/de/lex/compose/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Divider
+import androidx.compose.material.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import de.gnarly.compose.ui.slider.LabeledRangeSlider
@@ -20,7 +25,7 @@ class MainActivity : ComponentActivity() {
 		super.onCreate(savedInstanceState)
 
 		setContent {
-			Box(
+			Column(
 				modifier = Modifier.padding(16.dp)
 			) {
 				val steps = (0..100).step(10).toList()
@@ -34,9 +39,18 @@ class MainActivity : ComponentActivity() {
 					onRangeChanged = { lower, upper ->
 						lowerBound = lower
 						upperBound = upper
-						Log.i(LOG_TAG, "Updated range $lower <> $upper")
+						Log.i(LOG_TAG, "Updated selected range ${lowerBound..upperBound}")
 					},
 					modifier = Modifier.fillMaxWidth()
+				)
+
+				Spacer(modifier = Modifier.size(16.dp))
+				Divider()
+				Spacer(modifier = Modifier.size(16.dp))
+
+				Text(
+					text = "The selected range is ${lowerBound..upperBound}",
+					modifier = Modifier.align(Alignment.CenterHorizontally)
 				)
 			}
 		}
